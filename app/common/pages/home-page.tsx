8a780660191c340eb1a1bb0f32f39cbdbd79d7a9
Ricky <jonsoku.dev@gmail.com>
2025-03-06 23:29:27 +0900

    feat: 홈페이지 로더 업데이트 및 리다이렉트 로직 구현

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -2,19 +2,15 @@ import type { Route } from ".react-router/types/app/common/pages/+types/home-pag
 import { redirect } from "react-router";
 import { getServerClient } from "~/server";
 
-// export async function loader({ request }: Route.LoaderArgs) {
-//   const { supabase, headers } = getServerClient(request)
-//   const { data: { user } } = await supabase.auth.getUser();
+export async function loader({ request }: Route.LoaderArgs) {
+  const { supabase, headers } = getServerClient(request)
+  const { data: { user } } = await supabase.auth.getUser();
 
-//   if (!user) {
-//     return redirect("/auth/login");
-//   }
+  if (!user) {
+    return redirect("/auth/login");
+  }
 
-//   return redirect("/campaigns", {
-//     headers
-//   });
-// }
-
-export default function HomePage() {
-  return <div>HomePage</div>;
+  return redirect("/ebooks", {
+    headers
+  });
 }
